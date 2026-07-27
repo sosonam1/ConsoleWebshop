009d2ddc51d5254c77f29bd3d84d2d0456dc5f2c
sosonam1 <sosonam1@users.noreply.github.com>
2026-07-27 21:27:30 +0200

    Create ProductFileService
    work in progress

---
+++ src/main/java/org/example/ProductFileService.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductFileService {
+
+    private static final Path File_Path = Paths.get("data", "products.csv");
+
+    
+
+}
